7f87010e33ce16dc81cb4392233f145c9628c66a
Oscar Gullberg <oscar@filestar.com>
2022-10-10 22:07:49 +0200

    Improve store game

--- src/stores.ts
+++ src/stores.ts
@@ -9,20 +9,26 @@ export type Mobster = {
 	name: string;
 	active: boolean;
 };
-
 export type BroadcastState = {
 	lastChange: number;
 };
 
+const mobstersKey = 'mobsters';
+const timerConfigKey = 'timerConfig';
 const defaultTurnDurationMinutes = 15;
 const defaultTimerConfig = {
 	running: false,
 	initialSeconds: defaultTurnDurationMinutes * 60
 };
 
-export const timerConfig = writable<TimerConfig>(defaultTimerConfig);
-export const timer = writable<number>(defaultTimerConfig.initialSeconds);
-export const mobsters = writable<Mobster[]>([]);
+const storedMobsters = localStorage.getItem(mobstersKey);
+const initialMobsters = storedMobsters ? JSON.parse(storedMobsters) : [];
+const storedTimerConfig = localStorage.getItem(timerConfigKey);
+const initialTimerConfig = storedTimerConfig ? JSON.parse(storedTimerConfig) : defaultTimerConfig;
+
+export const timerConfig = writable<TimerConfig>(initialTimerConfig);
+export const timer = writable<number>(initialTimerConfig.initialSeconds);
+export const mobsters = createMobsterStore(initialMobsters);
 export const broadcast = writable<BroadcastState>({
 	lastChange: 0
 });
@@ -34,3 +40,33 @@ export const remaining = derived(timer, ($seconds) => ({
 		.toString()
 		.padStart(2, '0')
 }));
+export const activeMobster = derived(mobsters, ($mobsters) => $mobsters.find((m) => m.active));
+
+mobsters.subscribe((m) => {
+	if (m) {
+		localStorage.setItem(mobstersKey, JSON.stringify(m));
+	}
+});
+timerConfig.subscribe((t) => {
+	if (t) {
+		localStorage.setItem(timerConfigKey, JSON.stringify(t));
+	}
+});
+
+function createMobsterStore(value: Mobster[]) {
+	const { subscribe, set, update } = writable<Mobster[]>(value);
+	return {
+		subscribe,
+		set,
+		update,
+		changeMobster: () =>
+			update((mobsters) => {
+				const activeI = mobsters.findIndex((m) => m.active);
+				const newActiveI = activeI >= 0 ? (activeI + 1) % mobsters.length : 0;
+				return mobsters.map((m, index) => ({
+					...m,
+					active: index === newActiveI
+				}));
+			})
+	};
+}
